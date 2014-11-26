d487fc3c59001f2b60b93f86df2bad00d09e37d7
Ilya Mikhailov <ilya.mikhailov@hotmail.com>
2014-11-26 11:12:37 +0400

    resources suite fixes; version bump (pre release)

--- core/middleware/sourcejs-specs-linting/suites/missedResSuite.js
+++ core/middleware/sourcejs-specs-linting/suites/missedResSuite.js
@@ -3,16 +3,26 @@ module.exports = function(Validator) {
 	var fs = require('fs');
 	var path = require('path');
 
-	var isValidResourceURI = function(uri) {
+	var isSourceURI = function(uri) {
+		return !uri.indexOf('/source') || !uri.indexOf('/assets') || !uri.indexOf('/docs') || !uri.indexOf('/test');
+	};
+
+	var isValidResourceURI = function(uri, urlToSpec) {
 		if (!uri) return false;
-		var isInUser = fs.existsSync(path.join(global.app.get('user'), uri));
-		var isInMasterAppWeb = global.opts.core.masterApp && global.opts.core.masterApp.path
-			? fs.existsSync(path.join(global.opts.core.masterApp.path, uri))
-			: false;
-		var isInMasterAppMob = global.opts.core.masterApp && global.opts.core.masterApp.pathMob
-			? fs.existsSync(path.join(global.opts.core.masterApp.pathMob, uri))
-			: false;
-		return isInUser || isInMasterAppWeb || isInMasterAppMob;
+		if (isSourceURI(uri)) return true;
+		urlToSpec = urlToSpec || "";
+
+		var pathToUser = global.app.get('user') + "";
+		var pathToSpecDir = path.dirname(path.join(pathToUser, urlToSpec));
+		var pathToMasterApp = global.opts.core.masterApp ? global.opts.core.masterApp.path : undefined;
+		var pathToMasterMob = global.opts.core.masterApp ? global.opts.core.masterApp.pathMob : undefined;
+
+		var isInSpecDir = fs.existsSync(path.join(pathToSpecDir, uri));
+		var isInUser = fs.existsSync(path.join(pathToUser, uri));
+		var isInMasterApp = pathToMasterApp ? fs.existsSync(path.join(pathToMasterApp, uri)) : false;
+		var isInMasterMob = pathToMasterMob ? fs.existsSync(path.join(pathToMasterMob, uri)) : false;
+
+		return isInSpecDir || isInUser || isInMasterApp || isInMasterMob;
 	};
 
 	return Validator.create({
@@ -37,14 +47,14 @@ module.exports = function(Validator) {
 
 				var links = document.getElementsByTagName('link') || [];
 				for (var i = 0; i < links.length; i++) {
-					if (!isValidResourceURI(links[i].href)) {
+					if (!isValidResourceURI(links[i].href, urlToSpec)) {
 						return this.createException("ResourceNotFound", [links[i].href, urlToSpec]);
 					}
 				}
 
 				var images = document.getElementsByTagName('img') || [];
 				for (var i = 0; i < images.length; i++) {
-					if (!isValidResourceURI(images[i].src)) {
+					if (!isValidResourceURI(images[i].src.replace("file://", ""))) {
 						return this.createException("ResourceNotFound", [images[i].src, urlToSpec]);
 					}
 				}
